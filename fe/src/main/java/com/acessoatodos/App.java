fa3c1ba16193e54cde6bc66c8f490705b52dea3c
Daniel Fireman <danielfireman@gmail.com>
2016-08-29 10:54:56 -0300

    Metric collection when running in dev mode

--- fe/src/main/java/com/acessoatodos/App.java
+++ fe/src/main/java/com/acessoatodos/App.java
@@ -3,29 +3,47 @@ package com.acessoatodos;
 import org.jooby.Jooby;
 import org.jooby.Results;
 import org.jooby.json.Jackson;
+import org.jooby.metrics.Metrics;
 
 import com.acessoatodos.places.PlacesResource;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 
 /**
  * Entry point of FE server.
  */
 public class App extends Jooby {
-  // Simple HTTP endpoints.
-  // TODO(danielfireman): Endpoint addresses must be constants.
-  {
-	use(new Jackson());
+	{
+		// Jooby modules installation.
+		use(new Jackson());
 
-	assets("/favicon.ico", "favicon.ico");
+		// Resources from acessoatodos.
+		use(PlacesResource.class);
 
-    get("/ping", () -> Results.ok());
+		// When in dev, enable metrics collection.
+		on("dev", () -> {
+			use(new Metrics()
+					.request()
+					.threadDump()
+					.metric("memory", new MemoryUsageGaugeSet())
+					.metric("threads", new ThreadStatesGaugeSet())
+					.metric("gc", new GarbageCollectorMetricSet())
+					.metric("fs", new FileDescriptorRatioGauge()));
+		});
 
-    use(PlacesResource.class);
+		// TODO(danielfireman): Endpoint addresses must be constants.
+		// Static routes.
+		assets("/favicon.ico", "favicon.ico");
+		get("/ping", () -> Results.ok());
 
-    // Temporarily redirecting to github project page while we don't have a landing page.
-    get("/*", () -> Results.tempRedirect("https://github.com/danielfireman/acessoatodos"));
-  }
+		// Temporarily redirecting to github project page while we don't have a
+		// landing page.
+		get("/*", () -> Results.tempRedirect("https://github.com/danielfireman/acessoatodos"));
+	}
 
-  public static void main(final String[] args) throws Throwable {
-    run(App::new, args);
-  }
+	public static void main(final String[] args) throws Throwable {
+		run(App::new, args);
+	}
 }

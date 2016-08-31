d2ea3791462c9e434862ca8002fc0aa521aecd68
heienr <k-heiner@hotmail.com>
2016-08-31 07:19:03 -0300

    Merge branch 'master' of https://github.com/danielfireman/acessoatodos into add_dynamodb
    # Conflicts:
    #	fe/pom.xml
    #	fe/src/main/java/com/acessoatodos/App.java
    #	fe/src/main/java/com/acessoatodos/exception/AcessoATodosException.java
    #	fe/src/main/java/com/acessoatodos/exception/AcessoATodosExceptionHandler.java
    #	fe/src/main/java/com/acessoatodos/places/PlacesResource.java
    #	fe/src/main/java/com/acessoatodos/response/AcessoATodosResponse.java

--- fe/src/main/java/com/acessoatodos/App.java
+++ fe/src/main/java/com/acessoatodos/App.java
@@ -1,10 +1,18 @@
 package com.acessoatodos;
 
+import com.acessoatodos.acessibility.AcessibilityTableResource;
+import com.acessoatodos.places.PlacesResource;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.codahale.metrics.jvm.FileDescriptorRatioGauge;
+import com.codahale.metrics.jvm.GarbageCollectorMetricSet;
+import com.codahale.metrics.jvm.MemoryUsageGaugeSet;
+import com.codahale.metrics.jvm.ThreadStatesGaugeSet;
 import org.jooby.Jooby;
 import org.jooby.Results;
+import org.jooby.aws.Aws;
 import org.jooby.json.Jackson;
-
-import com.acessoatodos.places.PlacesResource;
+import org.jooby.metrics.Metrics;
+import org.jooby.whoops.Whoops;
 
 /**
  * Entry point of FE server.
@@ -15,32 +23,40 @@ public class App extends Jooby {
     {
         use(new Jackson());
 
-	assets("/favicon.ico", "favicon.ico");
+        // Resources from acessoatodos.
+        use(PlacesResource.class);
+        use(AcessibilityTableResource.class);
 
-    get("/ping", () -> Results.ok());
+        // TODO(danielfireman): Endpoint addresses must be constants.
+        // Static routes.
+        assets("/favicon.ico", "favicon.ico");
+        get("/ping", () -> Results.ok());
 
-		// Temporarily redirecting to github project page while we don't have a
-		// landing page.
-		get("/*", () -> Results.tempRedirect("https://github.com/danielfireman/acessoatodos"));
+        // Temporarily redirecting to github project page while we don't have a
+        // landing page.
+        get("/*", () -> Results.tempRedirect("https://github.com/danielfireman/acessoatodos"));
 
-		// Stuff that is enabled only in dev.
-		on("dev", () -> {
-			// Pretty page showing errors in development mode.
-			use(new Whoops());
+        // Stuff that is enabled only in dev.
+        on("dev", () -> {
+            // Pretty page showing errors in development mode.
+            use(new Whoops());
 
-			// When in dev, enable metrics collection.
-			use(new Metrics()
-					.request()
-					.threadDump()
-					.metric("memory", new MemoryUsageGaugeSet())
-					.metric("threads", new ThreadStatesGaugeSet())
-					.metric("gc", new GarbageCollectorMetricSet())
-					.metric("fs", new FileDescriptorRatioGauge()));
-		});
+            // When in dev, enable metrics collection.
+            use(new Metrics()
+                    .request()
+                    .threadDump()
+                    .metric("memory", new MemoryUsageGaugeSet())
+                    .metric("threads", new ThreadStatesGaugeSet())
+                    .metric("gc", new GarbageCollectorMetricSet())
+                    .metric("fs", new FileDescriptorRatioGauge()));
 
-	}
+            // TODO(danielfireman): Move the db endpoint to a flag.
+            use(new Aws().with(creds -> new AmazonDynamoDBClient(creds).withEndpoint("http://localhost:8000")));
+        });
 
-  public static void main(final String[] args) throws Throwable {
-    run(App::new, args);
-  }
+    }
+
+    public static void main(final String[] args) throws Throwable {
+        run(App::new, args);
+    }
 }

5375708feadfb907e306a1430dc02a0d6b76e5fd
heienr <k-heiner@hotmail.com>
2016-08-30 23:10:54 -0300

    Remove headers.

--- fe/src/main/java/com/acessoatodos/places/PlacesController.java
+++ fe/src/main/java/com/acessoatodos/places/PlacesController.java
@@ -1,34 +1,10 @@
-/**
- * This copy of Woodstox XML processor is licensed under the
- * Apache (Software) License, version 2.0 ("the License").
- * See the License for details about distribution rights, and the
- * specific rights regarding derivate works.
- *
- * You may obtain a copy of the License at:
- *
- * http://www.apache.org/licenses/
- *
- * A copy is also included in the downloadable source code package
- * containing Woodstox, in file "ASL2.0", under the same directory
- * as this file.
- */
 package com.acessoatodos.places;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.google.common.collect.Lists;
 import com.google.inject.Inject;
+
+import java.util.List;
 
 /**
  * This class is responsible to interact with external API, database and return view object. */
